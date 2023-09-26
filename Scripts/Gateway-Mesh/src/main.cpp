48625befba472e8ca08c137a5937da63dd9e6096
Mikhail Russell <mikhailrussell@outlook.com>
2023-09-26 18:29:51 +0200

    Added mesh codes

--- Scripts/Gateway-Mesh/src/main.cpp
+++ Scripts/Gateway-Mesh/src/main.cpp
@@ -1,11 +1,42 @@
 #include <Arduino.h>
+#include <esp_mesh.h>
+#include <esp_wifi.h>
+
 
 // put function declarations here:
 int myFunction(int, int);
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  
+  ESP_ERROR_CHECK(esp_netif_init());
+
+  /*  event initialization */
+  ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+  /*  Wi-Fi initialization */
+  wifi_init_config_t config = WIFI_INIT_CONFIG_DEFAULT();
+  ESP_ERROR_CHECK(esp_wifi_init(&config));
+  /*  register IP events handler */
+  esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &ip_event_handler, NULL);
+  ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_FLASH));
+  ESP_ERROR_CHECK(esp_wifi_start());
+  /* Enable the Mesh IE encryption by default */
+  mesh_cfg_t cfg = MESH_INIT_CONFIG_DEFAULT();
+  /* mesh ID */
+  memcpy((uint8_t *) &cfg.mesh_id, MESH_ID, 6);
+  /* channel (must match the router's channel) */
+  cfg.channel = CONFIG_MESH_CHANNEL;
+  /* router */
+  cfg.router.ssid_len = strlen(CONFIG_MESH_ROUTER_SSID);
+  memcpy((uint8_t *) &cfg.router.ssid, CONFIG_MESH_ROUTER_SSID, cfg.router.ssid_len);
+  memcpy((uint8_t *) &cfg.router.password, CONFIG_MESH_ROUTER_PASSWD,
+        strlen(CONFIG_MESH_ROUTER_PASSWD));
+  /* mesh softAP */
+  cfg.mesh_ap.max_connection = CONFIG_MESH_AP_CONNECTIONS;
+  memcpy((uint8_t *) &cfg.mesh_ap.password, CONFIG_MESH_AP_PASSWD,
+        strlen(CONFIG_MESH_AP_PASSWD));
+  ESP_ERROR_CHECK(esp_mesh_set_config(&cfg));
 }
 
 void loop() {
